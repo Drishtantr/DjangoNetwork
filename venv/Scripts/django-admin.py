aa308a3a00a7a43706567dfb264eacc9b507dbbb
drishtantr <drishtantregmi777@gmail.com>
2020-06-02 17:05:16 +0545

    First  Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\drish\desktop\projects all\django social network\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
